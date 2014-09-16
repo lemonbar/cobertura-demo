97ddf082fdf01a77b5b1151f92cf0f17d556cf45
lemonbar <limengabc@163.com>
2014-09-16 22:24:13 +0800

    first init

---
+++ src/main/java/com/lemon/android/App.java
@@ -0,0 +1,22 @@
+package com.lemon.android;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public void aaa() {
+        System.out.println("hello, me!");
+    }
+
+    public void bbb(int i) {
+        if (i > 0) {
+            System.out.println("greater than 0");
+        } else {
+            System.out.println("not greater than 0");
+        }
+    }
+}
